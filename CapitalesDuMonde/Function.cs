05cf8accaaba482a92d71bda49f919c9f3a31866
saladin <frq.salah@gmail.com>
2019-10-22 22:58:47 +0200

    Ajoutez des fichiers projet.

---
+++ CapitalesDuMonde/Function.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Alexa.NET.Request;
+using Alexa.NET.Response;
+using Amazon;
+using Amazon.Lambda.Core;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace CapitalesDuMonde
+{
+    public class Function
+    {
+        //Constantes 
+        private const string BUCKET_NAME = "listePaysfr";
+        private const string KEY_NAME = "monde.xml";
+        private const string SESSION_STATE_KEY = "skill-state";
+
+        private static readonly RegionEndpoint BucketRegion = RegionEndpoint.EUWest1;
+        public static IAmazonS3 S3Client;
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task<SkillResponse> FunctionHandler(SkillRequest input, ILambdaContext context)
+        {
+            S3Client = new AmazonS3Client(BucketRegion);
+            MemoryStream memory = new MemoryStream();
+            //Construct response
+            var response = new SkillResponse
+            {
+                Response = new ResponseBody()
+            };
+            response.Response.ShouldEndSession = false;
+            IOutputSpeech innerResponse = null;
+            string reponse = "";
+
+            //Récupérer la session
+            var playerSession = input.Session;
+            int playerScore = RestoreState(playerSession);
+
+            //Récupérer le fichier xml 
+            GetS3File(S3Client, memory);
+
+            var monde = new Monde();
+            var serialize = new XmlSerializer(typeof(Monde));
+            monde = (Monde)serialize.Deserialize(memory);
+
+            foreach(var m in monde.Continent)
+            {
+                LambdaLogger.Log(m.Nom);
+            }
+
+            innerResponse = new SsmlOutputSpeech();
+            ((SsmlOutputSpeech)innerResponse).Ssml = $"<speak>{reponse}</speak>";
+            response.Version = "1.0";
+            response.Response.OutputSpeech = innerResponse;
+            response.SessionAttributes = new Dictionary<string, object> { [SESSION_STATE_KEY] = playerScore };
+
+            return response;
+        }
+
+        /// <summary>
+        /// Récupérer le score stoquer dans la sessions
+        /// </summary>
+        /// <param name="session"></param>
+        /// <returns></returns>
+        public int RestoreState(Session session)
+        {
+            int score = 0;
+            if (session != null && session.New)
+            {
+                LambdaLogger.Log("New Session");
+            }
+            else
+            {
+                LambdaLogger.Log("Exesting Session");
+                //Récupérer l'objet stocké en session
+                var playerStateValue = session.Attributes.GetValueOrDefault(SESSION_STATE_KEY) ?? null;
+                try
+                {
+                    score = (int)playerStateValue;
+                }
+                catch (Exception)
+                {
+                    LambdaLogger.Log("Converstion Exception");
+                    return 0;
+                }
+            }
+            return score;
+
+        }
+
+        /// <summary>
+        /// récuperer le fichier stocker dans S3
+        /// </summary>
+        /// <param name="S3Client"></param>
+        public async void GetS3File(IAmazonS3 S3Client, MemoryStream memory)
+        {
+            var request = new GetObjectRequest
+            {
+                BucketName = BUCKET_NAME,
+                Key = KEY_NAME
+            };
+
+            try
+            {
+                LambdaLogger.Log("Get S3 file : ");
+                using (var s3Response = await S3Client.GetObjectAsync(request))
+                {
+                   // var memory = new MemoryStream();
+                    await s3Response.ResponseStream.CopyToAsync(memory);
+                }
+            }
+            catch (AmazonS3Exception e) when (e.StatusCode == HttpStatusCode.NotFound)
+            {
+                LambdaLogger.Log($"Error encountered ***. Message:'{0}' when writing an object {e.Message}");
+                throw new Exception("unable to load file from s3");
+            }
+            catch (Exception e)
+            {
+                LambdaLogger.Log($"Unknown encountered on server. Message:'{0}' when writing an object {e.Message}");
+                throw new Exception("Unknown encountered on server");
+            }
+
+        }
+
+    }
+
+}
+
---
+++ CapitalesDuMonde/Pays.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapitalesDuMonde
+{
+    class Pays
+    {
+
+    }
+}
+// REMARQUE : Le code généré peut nécessiter au moins .NET Framework 4.5 ou .NET Core/Standard 2.0.
+/// <remarks/>
+[System.SerializableAttribute()]
+[System.ComponentModel.DesignerCategoryAttribute("code")]
+[System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+[System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+public partial class Monde
+{
+
+    private MondeContinent[] continentField;
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlElementAttribute("continent")]
+    public MondeContinent[] Continent
+    {
+        get
+        {
+            return this.continentField;
+        }
+        set
+        {
+            this.continentField = value;
+        }
+    }
+}
+
+/// <remarks/>
+[System.SerializableAttribute()]
+[System.ComponentModel.DesignerCategoryAttribute("code")]
+[System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+public partial class MondeContinent
+{
+
+    private string nomContienent;
+
+    private MondeContinentPays[] listePays;
+
+    /// <remarks/>
+    public string Nom
+    {
+        get
+        {
+            return this.nomContienent;
+        }
+        set
+        {
+            this.nomContienent = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlArrayItemAttribute("pays", IsNullable = false)]
+    public MondeContinentPays[] Liste
+    {
+        get
+        {
+            return this.listePays;
+        }
+        set
+        {
+            this.listePays = value;
+        }
+    }
+}
+
+/// <remarks/>
+[System.SerializableAttribute()]
+[System.ComponentModel.DesignerCategoryAttribute("code")]
+[System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+public partial class MondeContinentPays
+{
+
+    private string nom;
+
+    private string capitale;
+
+    private string monnaie;
+
+    /// <remarks/>
+    public string Nom
+    {
+        get
+        {
+            return this.nom;
+        }
+        set
+        {
+            this.nom = value;
+        }
+    }
+
+    /// <remarks/>
+    public string Capitale
+    {
+        get
+        {
+            return this.capitale;
+        }
+        set
+        {
+            this.capitale = value;
+        }
+    }
+
+    /// <remarks/>
+    public string Monnaie
+    {
+        get
+        {
+            return this.monnaie;
+        }
+        set
+        {
+            this.monnaie = value;
+        }
+    }
+}
+
+
